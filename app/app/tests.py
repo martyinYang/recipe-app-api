3af9de4934659d4f8594dc729211c3fc0b2014d9
martyinYang <martyin.okuma@gmail.com>
2024-04-22 15:23:51 -0300

    testing django test

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    
+    def test_add_numbers(self):
+        res = calc.add(2, 2)
+        self.assertEqual(res, 4)
+
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(15, 10)
+
+        self.assertEqual(res, 5)
