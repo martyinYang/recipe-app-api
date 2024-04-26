0da95f437138b33046a1af30e2997a8b25f46395
martyinYang <martyin.okuma@gmail.com>
2024-04-25 21:46:20 -0300

    customizing admin display

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='test user'
+        )
+
+    def test_users_list(self):
+        url = reverse('admin:core_user_changelist')# url of the changelist, in this case is a page that shows the list of all registered users
+        res = self.client.get(url)
+
+        """since we have the list of users here we're checking if the users registered above are in this list"""
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+
+    def test_edit_user_page(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+
+    def test_create_user_page(self):
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
