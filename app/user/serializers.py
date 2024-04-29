bcb7cd32a513f7120b7b4da5c86bf8ecf1daa9c7
martyinYang <martyin.okuma@gmail.com>
2024-04-29 16:51:12 -0300

    implemented create user

---
+++ app/user/serializers.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta: # here we tell drf what is the model, fields and extra arguments that we want to pass to the serializer
+        model = get_user_model()
+        fields = ['email', 'password', 'name']
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}} #customization to the field
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
