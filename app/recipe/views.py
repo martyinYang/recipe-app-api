7c252e370b144e765bd03b60ece5a0df05fc27cf
martyinYang <martyin.okuma@gmail.com>
2024-05-07 16:33:02 -0300

    added recipe details

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,7 +6,7 @@ from core.models import Recipe
 from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -15,4 +15,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-id')
 
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
