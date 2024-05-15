722764200980783ab64a188ca4581e46e6afb96e
martyinYang <martyin.okuma@gmail.com>
2024-05-15 15:31:33 -0300

    put code in flake8 conditions

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -40,6 +40,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
                             mixins.UpdateModelMixin,
                             mixins.ListModelMixin,
